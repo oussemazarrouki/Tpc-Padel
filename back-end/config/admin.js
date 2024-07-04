712cd1e19c018d2b58ce15df44bd7b211ead1561
YoNexuss <oussemazarrouki01@gmail.com>
2024-07-04 12:07:55 +0100

    Merge main into your-branch-name

--- back-end/config/admin.js
+++ back-end/config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: '1e2ba921ca0424120201681eb884282c941ea0c20af3a07aef0fb69955d9e77a',
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: 'b3382405a423b8aca8f56e747eb144da906ccffa56a60b0bb87f8b14c309af92',
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: '977dcb02ad94e050cf4658ac76da4f4b3492fcefd76e27aba336c4fff0f64278',
     },
   },
   flags: {
